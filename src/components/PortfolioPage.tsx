69bc9476ed088c2267d490ec9d32956e98ea3dc1
lucasrbordigon <lucas.r.bordignon@gmail.com>
2023-01-28 08:33:53 -0300

    fixed the responsiveness

--- src/components/PortfolioPage.tsx
+++ src/components/PortfolioPage.tsx
@@ -7,34 +7,33 @@ export function PortfolioPage() {
       <div className='w-screen h-screen flex flex-col justify-between items-center'>
         <Navbar/> 
         <main className='flex flex-col items-center'>
-          <div className='flex items-center gap-16 mb-16'>
-            {/* <img className="h-40 w-40 border-t-2 border-l-2 border-l-yellow-500 border-b-2 border-b-yellow-500 rounded-full" src="https://media.licdn.com/dms/image/C4E03AQEH-yDRhYFSrw/profile-displayphoto-shrink_200_200/0/1640865016001?e=1678924800&v=beta&t=VI6ixd1Je4hfsFRX77wLay_Q2DjEjYGIfmZ-DzbY7LU" alt="" /> */}
+          <div className='flex flex-col items-center gap-16 mb-16 lg:flex-row'>
             <img typeof="image/svg+xml" src="/logo.svg" alt="" />
             <div className='flex-col justify-start'>
+
               <Heading><h1 className='text-gray-100 font-semibold'>LUCAS RODRIGUES BORDIGNON</h1></Heading>
               <Text size='sm'>LARANJAL PAULISTA - SÃO PAULO, BRAZIL</Text>
-              <br />
-              <br />
-              <Heading size='md'><h3 className='text-gray-200 font-light'>FRONT-END DEVELOPER</h3></Heading>
-              <div className='flex gap-1'>
 
+              <br />
+              <br />
+
+              <Heading size='md'><h3 className='text-gray-200 font-light'>FRONT-END DEVELOPER</h3></Heading>
+
+              <div className='flex gap-1'>
                 <img typeof="image/svg+xml" src="/react.svg" alt="" />
                 <img typeof="image/svg+xml" src="/javascript.svg" alt="" />
                 <img typeof="image/svg+xml" src="/html.svg" alt="" />
                 <img typeof="image/svg+xml" src="/css.svg" alt="" />
                 <img typeof="image/svg+xml" src="/tailwind.svg" alt="" />
                 <img typeof="image/svg+xml" src="/github.svg" alt="" />
-                <img typeof="image/svg+xml" src="/git.svg" alt="" />
-                
+                <img typeof="image/svg+xml" src="/git.svg" alt="" />                
               </div>
-              
-
             </div>
           </div>
             
           <div>
             <Text size='sm'>
-              <p >
+              <p className='block px-8'>
                 I was born in a small town in São Paulo, in the Campinas region. <br />
                 Since I was a child, I have been interested in computing, even though my family was not able to give me a PC, but I was able to study. <br />
                 That's when I passed a small entrance exam and started my studies in basic computing. That's where it all started. <br />
@@ -52,7 +51,7 @@ export function PortfolioPage() {
 
         </main>
 
-        <footer className='w-screen h-16 border-t-[1px] border-t-gray-800 flex items-center justify-around gap-[128px] pr-16 pl-16 lg:w-auto'>
+        <footer className='w-full h-16 border-t-[1px] border-t-gray-800 flex items-center justify-around gap-4 px-16'>
           <div className='flex items-center justify-around gap-2'>
             <Heading size='md'><p className='text-yellow-500'>LRB</p></Heading>
             <Text size='sm'><p className='text-gray-400'>© 2022 LRB</p></Text>
